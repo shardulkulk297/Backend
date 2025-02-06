741ef80aefd9901823b867bc39f4d21ed3d717ad
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-02-06 16:03:02 +0530

    Completed the FS MOdule

--- Node/FAsync.js
+++ Node/FAsync.js
@@ -1,9 +1,38 @@
 const {readFile, writeFile} = require('fs');
-
+console.log('start');
 readFile('./content/first.txt','utf8', (err, result)=>{
     if(err){
+        console.log(err)
         return;
     }
-    console.log(result);
+    
+
+    const first = result;
+    readFile('./content/sec.txt', 'utf8', (err, result)=>{
+
+        if(err){
+            console.log(err)
+            return;
+        }
+       
+        const sec = result;
+        
+    writeFile('./content/result-async.txt', `${first}, ${sec}`, (err, result)=>{
+        if(err)
+        {
+            console.log(err)
+            return
+        }
+
+        console.log('done with this task');
+        
+    });
+
+    })
+
+    
     
 });
+
+console.log('starting the next task');
+
--- Node/Fsynch.js
+++ Node/Fsynch.js
@@ -1,5 +1,7 @@
 const {readFileSync, writeFileSync} = require('fs');
 
+console.log('start')
+
 const first  = readFileSync('./content/first.txt', 'utf8');
 const sec = readFileSync('./content/sec.txt', 'utf8');
 
@@ -7,4 +9,8 @@ const sec = readFileSync('./content/sec.txt', 'utf8');
 // console.log(sec);
 
 writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${sec}`, {flag: 'a'});
+
+console.log("Done with the task")
+console.log('Starting the next one');
+
 
